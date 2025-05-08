63abbf1f19e929d679bc11d9c15d7e7028b866a8
TimlerTheTerp <165680770+TimlerTheTerp@users.noreply.github.com>
2025-05-07 21:51:02 -0400

    Update APIMain.js

--- APIMain.js
+++ APIMain.js
@@ -1,3 +1,8 @@
 function TodaysWeather() {
-    
+    let key = "910ff5ae063e6e027f6a48957062c78b";
+    let query = document.getElementById('');
+    let url = `https://api.weatherstack.com/current?access_key=${key}&query=${query}`;
 }
+
+
+function 
